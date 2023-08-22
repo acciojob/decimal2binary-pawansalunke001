a40abb6065a34872a4c0f51dc6172458b6816b08
Pawan Salunke <133101693+pawansalunke001@users.noreply.github.com>
2023-08-22 20:36:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function decimalToBinary(num) {
-  //Write you code here
-  
+
+	function decimalToBinary(decimalNumber) {
+    if (decimalNumber === 0) {
+        return '0';
+    }
+
+    let binary = '';
+    while (decimalNumber > 0) {
+        binary = (decimalNumber % 2) + binary;
+        decimalNumber = Math.floor(decimalNumber / 2);
+    }
+
+    return binary;
 }
+  
 
 window.decimalToBinary = decimalToBinary;
